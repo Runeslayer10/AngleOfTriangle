17ea3e586a468bd39f3faa37dbbbe9291619be1f
blhad3491 <blhad3491@7KXL6Y1.Student.UGDSB.ED>
2018-09-10 15:17:24 -0400

    How to find the missing side of a scalene triangle

---
+++ src/angleoftriange/AngleOfTriange.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package angleoftriange;
+import javax.swing.*;
+/**
+ *
+ * @author blhad3491
+ */
+public class AngleOfTriange {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String firstSideInput = JOptionPane.showInputDialog("Please Enter The First Side Length");
+        double firstSide = Double.parseDouble(firstSideInput);
+        String secondSideInput = JOptionPane.showInputDialog("Please Enter The Second Side Length");
+        double secondSide = Double.parseDouble(secondSideInput);
+        String firstAngleInput = JOptionPane.showInputDialog("Please Enter The Angle Across The Missing Side Length");
+        double angleInput = Double.parseDouble(firstAngleInput);
+        double finalSide = Math.sin(angleInput);
+        double answer = (firstSide+secondSide+finalSide/2);
+        Math.round(answer);
+        JOptionPane.showMessageDialog(null,"The Final Side is"+""+answer);
+    }
+    
+}

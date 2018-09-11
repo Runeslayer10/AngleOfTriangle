bbc9fa23ae2298529e4f1eab4aee522817bc8af1
Runeslayer10 <43037188+Runeslayer10@users.noreply.github.com>
2018-09-11 18:24:13 -0400

    Update AngleOfTriange.java

--- src/angleoftriange/AngleOfTriange.java
+++ src/angleoftriange/AngleOfTriange.java
@@ -16,16 +16,17 @@ public class AngleOfTriange {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String firstSideInput = JOptionPane.showInputDialog("Please Enter The First Side Length");
+        String firstSideInput = JOptionPane.showInputDialog("Please enter the first side length");
         double firstSide = Double.parseDouble(firstSideInput); //inputing the first side
-        String secondSideInput = JOptionPane.showInputDialog("Please Enter The Second Side Length");
+        String secondSideInput = JOptionPane.showInputDialog("Please enter the second side length");
         double secondSide = Double.parseDouble(secondSideInput); // inputing the second side
-        String firstAngleInput = JOptionPane.showInputDialog("Please Enter The Angle Across The Missing Side Length");
+        String firstAngleInput = JOptionPane.showInputDialog("Please enter the angle across from the missing side length");
         double angleInput = Double.parseDouble(firstAngleInput); // inputing the angle
         double finalSide = Math.sin(angleInput); // using the math.sin fuction on the angle inputed 
         double unroundedAnswer = ((firstSide+secondSide+finalSide)/2);
         double finalAnswer = Math.round(unroundedAnswer); // using the math.round fuction to round the final answer of the calculation
-        JOptionPane.showMessageDialog(null,"The Area Of The Triange is"+" "+finalAnswer);
+        String measure = JOptionPane.showInputDialog("Please enter your measurement of choice.");
+        JOptionPane.showMessageDialog(null,"The area of the triange is"+" "+finalAnswer+" "+ measure+" "+"squared");
     }
     
 }
